1980bee64e207666e6f73a3ea1dad16951cc960c
Mrunali <mrunalishinde2805@gmail.com>
2024-06-21 22:17:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import ContactForm from './ContactForm';
+
+function App() {
+  return (
+    <div className="App">
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ContactForm.js
@@ -0,0 +1,70 @@
+// src/ContactForm.js
+import React, { useState } from 'react';
+import './ContactForm.css';
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form Data Submitted: ', formData);
+    // Here you can add the logic to send the form data to a server or an API.
+    // Reset the form after submission
+    setFormData({ name: '', email: '', message: '' });
+  };
+
+  return (
+    <div className="contact-form-container">
+      <h1>Contact us</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="Name"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            placeholder="Enter your message"
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
